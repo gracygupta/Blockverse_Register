6601c671cb73e23096cb73427235ccebfed28b05
gracygupta <gracy2112004@akgec.ac.in>
2022-12-16 04:07:36 -0800

    Project Setup

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const ejs = require("ejs");
+// const expressLayouts = require("express-ejs-layouts");
+
+// require("./controller/passportAuth")(passport);
+require("dotenv").config();
+require("./db/conn");
+const port = process.env.PORT;
+const app = express();
+
+//middlewares
+app.use(passport.initialize());
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//importing routes
+// const routes = require("./routes/router");
+
+//Routes
+// app.use(routes);
+
+app.listen(port, function () {
+  console.log(`Server is up at ${port}`);
+});
